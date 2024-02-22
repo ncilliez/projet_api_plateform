acd8d89c0ce2894dbc91285bf02ea8e878767005
Nicolas Cilliez <ncilliez@gmail.com>
2024-02-22 10:49:43 +0100

    apiPlateform

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\ProfileRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: ProfileRepository::class)]
+#[ApiResource()]
 class Profile
 {
     #[ORM\Id]
